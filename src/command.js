d1e0a7703b5a7f6ac57d3d1ffed542a9fd09e7fa
René Kooi <rene@kooi.me>
2015-04-11 11:58:02 +0200

    add user ranks restriction option to `@commands`

--- src/command.js
+++ src/command.js
@@ -6,6 +6,21 @@ export default function command(commandName, ...argTypes) {
     if (!prototype._commandMethods) prototype._commandMethods = []
     prototype._commandMethods.push(key)
 
+    let opts = {}
+    if (typeof argTypes[argTypes.length - 1] === 'object') {
+      opts = argTypes.pop()
+    }
+
+    if (opts.ranks) {
+      let action = descriptor.value
+      descriptor.value = function (...args) {
+        const userType = args[0].special
+        if (opts.ranks.some(rank => userType.indexOf(rank) > -1)) {
+          return action.apply(this, args)
+        }
+      }
+    }
+
     descriptor.value.commandName = commandName
     descriptor.value.commandParameters = argTypes
 
