1522929fc47aa04d46859f531c134d8c5fa8aa88
René Kooi <rene@kooi.me>
2015-04-12 20:39:44 +0200

    add reddit quote module

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import WololoBot from './index'
 import Version from './modules/Version'
 import Florins from './modules/Florins'
+import RandomReddit from './modules/RandomReddit'
 import { readFileSync as readFile } from 'fs'
 
 export default function main(confile = 'config.json') {
@@ -9,6 +10,7 @@ export default function main(confile = 'config.json') {
 
   new Version(wb, {})
   new Florins(wb, {})
+  new RandomReddit(wb, conf.RandomReddit || { sub: 'random' })
 
   wb.connect()
 
