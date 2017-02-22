04110cc9bfefbd0fd1981ff6ff881f11d3c0563c
René Kooi <renee@kooi.me>
2017-02-22 16:01:56 +0100

    Add florins reservation migration.

---
+++ migrations/20170222144337_florins_reserve.js
@@ -0,0 +1,11 @@
+exports.up = (db) =>
+  db.schema.createTable('florinReservations', (table) => {
+    table.increments('id').primary()
+    table.string('username', 50).index()
+    table.integer('amount').index()
+    table.timestamp('time').defaultTo(db.fn.now())
+    table.string('purpose', 50)
+  })
+
+exports.down = (knex) =>
+  db.schema.dropTable('florinReservations')
